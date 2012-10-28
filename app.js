3abe2a057bd579604302fbf2717255fa089e2795
Rob DeCarlo <rdecarlo73@gmail.com>
2012-10-28 01:34:18 -0400

    added some AppFog bits, still not connecting to MongLab

--- app.js
+++ app.js
@@ -8,7 +8,10 @@ app.configure(function () {
     app.use(express.bodyParser());
 });
 
-//app.get('/', prayers.findRandom);
+app.get('/', function (req, res) {
+	res.writeHead(200, {'Content-Type': 'text/html'});
+  	res.end('Hello from AppFog!');
+});
 app.get('/prayers', prayers.findAll);
 app.get('/prayers/:id', prayers.findByIndex);
 //app.post('/prayers', prayers.addPrayer);
